e5470ec472193ede0e7814646be60d5c1c41e566
Federico Paparoni <federico.paparoni@gmail.com>
2018-03-16 16:40:32 +0100

    Update step1.py

--- step1/step1.py
+++ step1/step1.py
@@ -7,6 +7,7 @@ def main(args):
     pushername = args.get("pusher").get("name")
     print(compare)
     print("Pusher "+pushername+" "+pusheremail)
-    r = requests.post("https://us-central1-hello-cloud-196820.cloudfunctions.net/helloCloud", data={'compare': compare, 'pushername': pushername, 'pusheremail': pusheremail})
+    r = requests.post("https://us-central1-hello-cloud-196820.cloudfunctions.net/helloCloud", 
+                      data={'compare': compare, 'pushername': pushername, 'pusheremail': pusheremail})
     print(r.status_code, r.reason)
     return {"compare": compare}
